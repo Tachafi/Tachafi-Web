7badff460f2b575cdab0bf69eaf83769e547b88e
Ben <m3hdi.b3n@gmail.com>
2024-10-27 17:46:54 +0100

    Deploy Version 0.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9ae747be34395dbbbf6ab647f26cdd0c",
+const RESOURCES = {"flutter_bootstrap.js": "69a691721df4c8208db7d60177ef71dc",
 "version.json": "548718205dba3bcb1a8da020e8b14ae8",
 "index.html": "94d121073498f1a2db92c08ab99ed5a1",
 "/": "94d121073498f1a2db92c08ab99ed5a1",
-"main.dart.js": "b1d2171c460d5a2c05037e7819c28d7d",
+"main.dart.js": "588e0e849a3a6b7f2722757127ae73f1",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
